5650b940514ac5ae361f444dbb9949464994e944
cedric tchana <tchanacool@gmail.com>
2022-11-02 16:16:57 +0100

    TD cours terminés !!

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/Home'
-
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/survey/:questionNumber">
+            <Survey />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
+      </Router>   
   </React.StrictMode>,
   document.getElementById('root')
 )
